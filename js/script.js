46828c2d3da7e69e51505863a287c565ca9dbc48
Nioaes <whitney.a12221@gmail.com>
2021-08-27 19:50:00 -0400

    Added double quotations to the height in the javascript pokemonlist

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 let pokemonList = [
   {
     name: 'Grookey',
-    height: 1'00",
+    height: "1'00",
     type: 'Grass'
   },
 
   {
     name: 'Scorbunny',
-    height: 1'00",
+    height: "1'00",
     type: 'Fire',
   },
 
   {
     name: 'Sobble',
-    height: 1'00",
+    height: "1'00",
     type: 'Water'
   }
 ];
