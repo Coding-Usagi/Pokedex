b81cb617322b152283bfbdcc6852ed8a5cf5152b
Nioaes <whitney.a12221@gmail.com>
2021-08-27 13:02:46 -0400

    Deleted original JS script and added a PokemonList with 3 Pokemon

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let pokemonList = [
+  {
+    name: 'Grookey',
+    height: 1'00",
+    type: 'Grass'
+  },
+
+  {
+    name: 'Scorbunny',
+    height: 1'00",
+    type: 'Fire',
+  },
+
+  {
+    name: 'Sobble',
+    height: 1'00",
+    type: 'Water'
+  }
+];
