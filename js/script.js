1747e70303fce2b567d79485a70081b94230cec4
Nioaes <whitney.a12221@gmail.com>
2021-09-02 11:53:00 -0400

    Added spacing and a note to myself to remember

--- js/script.js
+++ js/script.js
@@ -17,8 +17,8 @@ let pokemonList = [
     type: 'Water'
   }
 ];
-// this doesn't have spacing yet
+// Apparently double quotes in between add spacing???
 for (let i = 0; i < pokemonList.length; i++){
   console.log(pokemonList[i])
-  document.write(pokemonList[i].name + pokemonList[i].height)
+  document.write(pokemonList[i].name +  " " + pokemonList[i].height + " ")
 }
