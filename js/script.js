c3634bea0086e2b0fcbd218d4afd551fbbfe2701
Nioaes <whitney.a12221@gmail.com>
2021-09-08 13:09:26 -0400

    Turned for loop into notes and currently working on forEach loop, somehow the document.write is a mess

--- js/script.js
+++ js/script.js
@@ -17,15 +17,20 @@ let pokemonList = [
     type: 'Water'
   }
 ];
-// Apparently double quotes in between add spacing???
-for (let i = 0; i < pokemonList.length; i++){
-  if (pokemonList[i].height > 1.00){
-    result = "Wow that's big!";
-  } else {
-    result = "";
-  }
 
-  console.log(pokemonList[i])
-  document.write(pokemonList[i].name +  " " + pokemonList[i].height + " ");
-  document.write(result + " ")
-}
+pokemonList.forEach(function(pokemon){
+  document.write('<p>' + pokemon + '</p>');
+  console.log(pokemon.name + pokemon.height + pokemon.type);
+});
+// Apparently double quotes in between add spacing???
+//for (let i = 0; i < pokemonList.length; i++){
+  //if (pokemonList[i].height > 1.00){
+    //result = "Wow that's big!";
+  //} else {
+    //result = "";
+  //}
+
+  //console.log(pokemonList[i])
+  //document.write(pokemonList[i].name +  " " + pokemonList[i].height + " ");
+  //document.write(result + " ")
+//}
